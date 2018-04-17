0ed556907ae1f95dca7307174c1565401ff07501
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-04-17 11:57:23 +0100

    removed first 2 tests as they are now redundant, new tests passes. Step one completed

--- src/main/scala/SecondLargestNumber.scala
+++ src/main/scala/SecondLargestNumber.scala
@@ -2,6 +2,10 @@ object SecondLargestNumber {
 
   def finder(register : List[Int], n : Int) : Int  = {
     val register = List(1,2,3,4,5)
-    n
+
+val register2 = register.distinct.sorted
+  register2(register2.length-n)
+
+
   }
 }
